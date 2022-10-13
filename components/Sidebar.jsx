8834e5e0b41d777e39e803443ccbb0a19f6f1253
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-10-13 20:07:27 -0300

    enter

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,34 +1,43 @@
 import React, { useState } from 'react';
 import {BiMenuAltRight, BiMenuAltLeft} from 'react-icons/bi';
+import {FiMenu,FiX,FiGitlab   } from 'react-icons/fi';
 import { IconContext } from 'react-icons';
 
-function Sidebar() {
-  const [sidebar, setSidebar] = useState(false);
+const Menu = () => (
+  <>
+  <p><a href='#Pricing'>Planos</a></p>
+  <p><a href='#Channels'>Vantagens</a></p>
+  <p><a href='#freetrial'>Teste</a></p>
+  </>
+)
 
+function Sidebar() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-      <header class="text-white sidebar-bg body-font">
-        <div class="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
-          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span class="ml-3 text-xl text-white">Tailblocks</span>
-          </a>
-          <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-gray-100">Eletronicos</a>
-            <a class="mr-5 hover:text-gray-100">Roubas</a>
-            <a class="mr-5 hover:text-gray-100">Sexy shop</a>
-            <a class="mr-5 hover:text-gray-100">Maquiagem</a>
-          </nav>
-          <li className='inline-flex items-center border-0 py-1 focus:outline-none mt-4 md:mt-0'>
-            <BiMenuAltRight onClick={showSidebar} className="text-3xl"/>
-          </li>
-        </div>
-      </header>
+      <div className='cine-club__navbar bg-gradient-to-r from-red-500 via-purple-500/80 to-pink-500/80 backdrop-blur-[10px]'>
+
+      <div className='cine-club__navbar-links'>
+          <div className='cine-club__navbar-links_logo'>
+              <div className='logo'><FiGitlab style={{ fontSize: '50' }} /></div>
+          </div>
+
+          <div className='cine-club__navbar-links_container'>
+            <Menu />
+          </div>
+          
+      </div>
+
+      <div className='cine-club__navbar-menu'>
+        <FiMenu color="#f" size="27" onClick={showSidebar}/>
+      </div>      
+
+</div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
             <li className='navbar-toggle'>
